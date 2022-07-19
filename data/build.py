205bcdca3802766c3e096321b343692387b080d9
Ehab <1152012166@qq.com>
2022-07-19 16:06:11 +0800

    data

--- data/build.py
+++ data/build.py
@@ -33,29 +33,29 @@ RGB_statistics = {
     }
 }
 
-'''
-try:
-    from torchvision.transforms import InterpolationMode
+
+# try:
+#     from torchvision.transforms import InterpolationMode
 
 
-    def _pil_interp(method):
-        if method == 'bicubic':
-            return InterpolationMode.BICUBIC
-        elif method == 'lanczos':
-            return InterpolationMode.LANCZOS
-        elif method == 'hamming':
-            return InterpolationMode.HAMMING
-        else:
-            # default bilinear, do we want to allow nearest?
-            return InterpolationMode.BILINEAR
+#     def _pil_interp(method):
+#         if method == 'bicubic':
+#             return InterpolationMode.BICUBIC
+#         elif method == 'lanczos':
+#             return InterpolationMode.LANCZOS
+#         elif method == 'hamming':
+#             return InterpolationMode.HAMMING
+#         else:
+#             # default bilinear, do we want to allow nearest?
+#             return InterpolationMode.BILINEAR
 
 
-    import timm.data.transforms as timm_transforms
+#     import timm.data.transforms as timm_transforms
 
-    timm_transforms._pil_interp = _pil_interp
-except:
-    from timm.data.transforms import _pil_interp
-'''
+#     timm_transforms._pil_interp = _pil_interp
+# except:
+#     from timm.data.transforms import _pil_interp
+
 
 
 def build_loader(config):
@@ -155,29 +155,29 @@ def build_dataset(is_train, config):
     dataset = LT_Dataset(root, txt, transform)
     n_class = len(dataset)
     return dataset, n_class
-    '''
-    if config.DATA.DATASET == 'imagenet':
-        prefix = 'train' if is_train else 'val'
-        if config.DATA.ZIP_MODE:
-            ann_file = prefix + "_map.txt"
-            prefix = prefix + ".zip@/"
-            dataset = CachedImageFolder(config.DATA.DATA_PATH, ann_file, prefix, transform,
-                                        cache_mode=config.DATA.CACHE_MODE if is_train else 'part')
-        else:
-            root = os.path.join(config.DATA.DATA_PATH, prefix)
-            dataset = datasets.ImageFolder(root, transform=transform)
-        n_class = 1000
-    elif config.DATA.DATASET == 'imagenet22K':
-        prefix = 'ILSVRC2011fall_whole'
-        if is_train:
-            ann_file = prefix + "_map_train.txt"
-        else:
-            ann_file = prefix + "_map_val.txt"
-        dataset = IN22KDATASET(config.DATA.DATA_PATH, ann_file, transform)
-        n_class = 21841
-    else:
-        raise NotImplementedError("We only support ImageNet Now.")
-    '''
+    
+    # if config.DATA.DATASET == 'imagenet':
+    #     prefix = 'train' if is_train else 'val'
+    #     if config.DATA.ZIP_MODE:
+    #         ann_file = prefix + "_map.txt"
+    #         prefix = prefix + ".zip@/"
+    #         dataset = CachedImageFolder(config.DATA.DATA_PATH, ann_file, prefix, transform,
+    #                                     cache_mode=config.DATA.CACHE_MODE if is_train else 'part')
+    #     else:
+    #         root = os.path.join(config.DATA.DATA_PATH, prefix)
+    #         dataset = datasets.ImageFolder(root, transform=transform)
+    #     n_class = 1000
+    # elif config.DATA.DATASET == 'imagenet22K':
+    #     prefix = 'ILSVRC2011fall_whole'
+    #     if is_train:
+    #         ann_file = prefix + "_map_train.txt"
+    #     else:
+    #         ann_file = prefix + "_map_val.txt"
+    #     dataset = IN22KDATASET(config.DATA.DATA_PATH, ann_file, transform)
+    #     n_class = 21841
+    # else:
+    #     raise NotImplementedError("We only support ImageNet Now.")
+    
     
     
 
@@ -199,42 +199,41 @@ def build_transform(is_train, config=None, rgb_mean=RGB_statistics['default']['m
             transforms.ToTensor(),
             transforms.Normalize(rgb_mean, rgb_std)
         ])
-    '''
-    resize_im = config.DATA.IMG_SIZE > 32
-    if is_train:
-        # this should always dispatch to transforms_imagenet_train
-        transform = create_transform(
-            input_size=config.DATA.IMG_SIZE,
-            is_training=True,
-            color_jitter=config.AUG.COLOR_JITTER if config.AUG.COLOR_JITTER > 0 else None,
-            auto_augment=config.AUG.AUTO_AUGMENT if config.AUG.AUTO_AUGMENT != 'none' else None,
-            re_prob=config.AUG.REPROB,
-            re_mode=config.AUG.REMODE,
-            re_count=config.AUG.RECOUNT,
-            interpolation=config.DATA.INTERPOLATION,
-        )
-        if not resize_im:
-            # replace RandomResizedCropAndInterpolation with
-            # RandomCrop
-            transform.transforms[0] = transforms.RandomCrop(config.DATA.IMG_SIZE, padding=4)
-        return transform
+    
+    # resize_im = config.DATA.IMG_SIZE > 32
+    # if is_train:
+    #     # this should always dispatch to transforms_imagenet_train
+    #     transform = create_transform(
+    #         input_size=config.DATA.IMG_SIZE,
+    #         is_training=True,
+    #         color_jitter=config.AUG.COLOR_JITTER if config.AUG.COLOR_JITTER > 0 else None,
+    #         auto_augment=config.AUG.AUTO_AUGMENT if config.AUG.AUTO_AUGMENT != 'none' else None,
+    #         re_prob=config.AUG.REPROB,
+    #         re_mode=config.AUG.REMODE,
+    #         re_count=config.AUG.RECOUNT,
+    #         interpolation=config.DATA.INTERPOLATION,
+    #     )
+    #     if not resize_im:
+    #         # replace RandomResizedCropAndInterpolation with
+    #         # RandomCrop
+    #         transform.transforms[0] = transforms.RandomCrop(config.DATA.IMG_SIZE, padding=4)
+    #     return transform
 
-    t = []
-    if resize_im:
-        if config.TEST.CROP:
-            size = int((256 / 224) * config.DATA.IMG_SIZE)
-            t.append(
-                transforms.Resize(size, interpolation=_pil_interp(config.DATA.INTERPOLATION)),
-                # to maintain same ratio w.r.t. 224 images
-            )
-            t.append(transforms.CenterCrop(config.DATA.IMG_SIZE))
-        else:
-            t.append(
-                transforms.Resize((config.DATA.IMG_SIZE, config.DATA.IMG_SIZE),
-                                  interpolation=_pil_interp(config.DATA.INTERPOLATION))
-            )
+    # t = []
+    # if resize_im:
+    #     if config.TEST.CROP:
+    #         size = int((256 / 224) * config.DATA.IMG_SIZE)
+    #         t.append(
+    #             transforms.Resize(size, interpolation=_pil_interp(config.DATA.INTERPOLATION)),
+    #             # to maintain same ratio w.r.t. 224 images
+    #         )
+    #         t.append(transforms.CenterCrop(config.DATA.IMG_SIZE))
+    #     else:
+    #         t.append(
+    #             transforms.Resize((config.DATA.IMG_SIZE, config.DATA.IMG_SIZE),
+    #                               interpolation=_pil_interp(config.DATA.INTERPOLATION))
+    #         )
 
-    t.append(transforms.ToTensor())
-    t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
-    return transforms.Compose(t)
-    '''
+    # t.append(transforms.ToTensor())
+    # t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
+    # return transforms.Compose(t)
